9e0225c655103141628511b296601c4fe37453b3
lizduran <lizeth.durang@hotmail.com>
2021-12-09 21:20:32 -0600

    Codigo documentado

--- Scanner.h
+++ Scanner.h
@@ -1,7 +1,12 @@
-/**
- * Análisis Léxico
- * 
- **/
+/*
+* Analizador Léxico
+* Compiladores 2022-1
+* Grupo: 1
+* Integrantes: Durán González Lizeth        |   García Meneses jeremy   
+            Mendoza de la Vega Elizabeth    |   Parada Pérez Jesús Bryan
+*
+* Descripción: Clase Scanner hereda de clase yyFlexLexer
+*/
 
 #ifndef __SCANNER_HPP__
 #define __SCANNER_HPP__ 1
@@ -17,9 +22,7 @@ namespace COMP
     class Scanner : public yyFlexLexer
     {
     public:
-        Scanner(std::istream *in) : yyFlexLexer(in)
-        {
-        };
+        Scanner(std::istream *in) : yyFlexLexer(in){};
 
         using FlexLexer::yylex;
         virtual int yylex();
--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/*
+* Analizador Léxico
+* Compiladores 2022-1
+* Grupo: 1
+* Integrantes: Durán González Lizeth        |   García Meneses jeremy   
+            Mendoza de la Vega Elizabeth    |   Parada Pérez Jesús Bryan
+*
+* Descripción:  Llamada a los métodos del análisis léxico. Código que
+*               abre el archivo prueba y lo pasa al analizador léxico.
+*/
+
 #include <iostream>
 #include <fstream>
 #include "Scanner.h"
@@ -11,13 +22,18 @@ int main(int argc, char *argv[]){
 		return EXIT_FAILURE;
 
 	}
-
+	/*Crea el objeto archivo*/
 	filebuf fb;
 	fb.open(string(argv[1]), ios::in);
+	
+	/*Lectura del archivo que contiene el código*/
 	istream in(&fb);
-	Scanner lexer(&in);
+	
+	/*Se pasa el contenido del archivo al lexer*/
+	Scanner lexer(&in); 
 	int token = lexer.yylex();
 
+	/*Lectura de tokens*/
 	while(token != 0){
 		cout<<token <<" , "<< lexer.YYText()<<endl;
 		token = lexer.yylex();
